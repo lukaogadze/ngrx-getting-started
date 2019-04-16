754153200e5c26059b36a69f347427c41b999fcd
Luka Oghadze <loghadze@justice.gov.ge>
2019-04-16 15:39:48 +0400

    added store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 
-// Imports for loading & configuring the in-memory web api
+
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {ProductData} from './products/product-data';
 
@@ -14,9 +14,11 @@ import {MenuComponent} from './home/menu.component';
 import {WelcomeComponent} from './home/welcome.component';
 import {PageNotFoundComponent} from './home/page-not-found.component';
 
-/* Feature Modules */
+
 import {UserModule} from './user/user.module';
 import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
     imports: [
@@ -25,7 +27,12 @@ import {StoreModule} from '@ngrx/store';
         HttpClientInMemoryWebApiModule.forRoot(ProductData),
         UserModule,
         AppRoutingModule,
-        StoreModule.forRoot({})
+        StoreModule.forRoot({}),
+        StoreDevtoolsModule.instrument({
+            name: 'APM Demo App DevTools',
+            maxAge: 25,
+            logOnly: environment.production
+        })
     ],
     declarations: [
         AppComponent,
